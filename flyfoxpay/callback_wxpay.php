ffc6c6f3cb70fcb5d239b04db8d24fef7c1cc762
黃柏澄(風雨) <wartw@users.noreply.github.com>
2019-05-23 11:08:32 +0800

    Add files via upload

---
+++ flyfoxpay/callback_wxpay.php
@@ -0,0 +1,90 @@
+<?php
+use WHMCS\Database\Capsule;
+# Required File Includes
+include("../../../init.php");
+include("../../../includes/functions.php");
+include("../../../includes/gatewayfunctions.php");
+include("../../../includes/invoicefunctions.php");
+
+$gatewaymodule = "flyfoxpay_wxpay";
+$GATEWAY       = getGatewayVariables($gatewaymodule);
+
+if (!$GATEWAY["type"]) die("fail");
+
+$security['orderid'] = $_REQUEST['orderid'];
+//手续费
+$fee = 0;
+$url = "https://sc-i.pw/api/check/";//API位置
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0');
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(
+ array("key"=>$GATEWAY["key"], //商家KEY
+       "id"=>$GATEWAY["mchid"], //商家ID
+       "mail"=>$GATEWAY["account"], //商家EMAIL
+       "trade_no"=>$_REQUEST['orderid'], //商家訂單ID
+       ))); 
+$output = curl_exec($ch); 
+curl_close($ch);
+/*
+回傳格式:
+//成功
+{"status":"200","status_trade":"noapy","sign":"90e5f1f7ef87cd2e43729ba4378656b5"}
+{"status":"200","trade_no":"1278217527512","type":"o_alipay","status_trade":"payok","sign":"*****"}
+//以下為錯誤項目
+{"status":"404","error":"未設置KEY或是ID或MAIL"}
+{"status":"400","error":"請檢查ID或是KEY或MAIL是否有誤"}
+{"status":"416","error":"請檢查訂單ID是否有誤"}
+*/ 
+$security1  = array();
+
+$security1['mchid']      = $GATEWAY["mchid"];//商家ID
+
+$security1['status']        = "7";//驗證，請勿更改
+
+$security1['mail']      = $GATEWAY["account"];//商家EMAIL
+
+$security1['trade_no']      = $security['orderid'];//商家訂單ID
+
+foreach ($security1 as $k=>$v)
+
+{
+
+    $o.= "$k=".($v)."&";
+
+}
+
+$sign1 = md5(substr($o,0,-1).$GATEWAY["key"]);//**********請替換成商家KEY
+$json=json_decode($output, true);
+$security['out_trade_no'] = $json['customize1'];
+$security['trade_no'] = $json['trade_no'];
+$amount= $json['money'];
+$typesfa= $json['type'];
+if($typesfa=="o_alipay"){$typess='flyfoxpay_alipay';}elseif($typesfa=="o_wxpay"){$typess='flyfoxpay_wxpay';}
+if($json['sign']==$sign1){
+    $invoiceid = checkCbInvoiceID($security['out_trade_no'], $GATEWAY["name"]);
+    checkCbTransID($security['out_trade_no']);
+	function convert_helper($invoiceid,$amount){
+    $setting = Capsule::table("tblpaymentgateways")->where("gateway",$gatewaymodule)->where("setting","convertto")->first();
+    ///系统没多货币 , 直接返回
+    if (empty($setting)){ return $amount; }
+    
+    
+    ///获取用户ID 和 用户使用的货币ID
+    $data = Capsule::table("tblinvoices")->where("id",$invoiceid)->get()[0];
+    $userid = $data->userid;
+    $currency = getCurrency( $userid );
+
+    /// 返回转换后的
+    return  convertCurrency( $amount , $setting->value  ,$currency["id"] );
+}
+	  $amount = convert_helper( $invoiceid, $fee);
+    addInvoicePayment($invoiceid,$security['trade_no'],trim($amount),$fee,$typess);
+    logTransaction($GATEWAY["name"], $_REQUEST, "Successful");
+    echo 'success';
+} else {
+    echo 'fail'.$output.'/'.$sign1;
+}
---
+++ flyfoxpay_wxpay.php
@@ -0,0 +1,117 @@
+<?php
+use WHMCS\Database\Capsule;
+
+function flyfoxpay_wxpay_MetaData() {
+    return array(
+        'DisplayName' => '翔狐科技(支付宝)',
+        'APIVersion' => '1.1',
+    );
+}
+
+function flyfoxpay_wxpay_config() {
+    $configarray = array(
+        "FriendlyName"  => array(
+            "Type"  => "System",
+            "Value" => "翔狐科技(微信)"
+        ),
+        "account"  => array(
+            "FriendlyName" => "商户邮箱",
+            "Type"         => "text",
+            "Size"         => "32",
+        ),
+        "key" => array(
+            "FriendlyName" => "商户KEY",
+            "Type"         => "password",
+            "Size"         => "32",
+        ),
+        "mchid" => array(
+            "FriendlyName" => "商户ID",
+            "Type"         => "text",
+            "Size"         => "32",
+        )
+    );
+
+    return $configarray;
+}
+
+function flyfoxpay_wxpay_link($params) {
+    if($_REQUEST['alipaysubmit'] == 'yes'){
+		$security  = array();
+       $security['id']      = $params['mchid'];
+       $security['mail']    = $params['account'];
+       $security['keys']        = md5($params['key']);
+       $security['trade_name']       = 'whmcs'.time();
+       $security['trade_no']      = 'whmcs'.time();
+       $security['customize1']      = $params['invoiceid'];
+		 $security['amount']      = $params['amount'];
+		$security['type']      = 'o_wxpay';
+		 $security['go']      = '1';
+		$security['return']      = $params['returnurl'];
+		$o='';
+$sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='https://sc-i.pw/api/' method='post'>";
+	   while (list ($key, $val) = each ($security)) {
+            $sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/>";
+       }
+       $sHtml = $sHtml."<input type='submit' value='正在跳转'></form>";
+	   $sHtml = $sHtml."<script>document.forms['alipaysubmit'].submit();</script>";
+	   exit($sHtml);
+	}
+    if(stristr($_SERVER['PHP_SELF'],'viewinvoice')){
+		return '<form method="post" id=\'alipaysubmit\'><input type="hidden" name="alipaysubmit" value="yes"></form><button type="button" class="btn btn-danger btn-block" onclick="document.forms[\'alipaysubmit\'].submit()">使用微信支付</button>';
+    }else{
+         return '<img style="width: 150px" src="'.$params['systemurl'].'/modules/gateways/flyfoxpay_wxpay/alipay.png" alt="微信支付" />';
+    }
+
+}
+
+//LeoTIME
+/****************************************************************
+ * 
+ *       Author:Leo
+ * 
+ *       Made for daimiyun.cn
+ * 
+ * **************************************************************/
+if(!function_exists("autogetamount")){
+function autogetamount($params){
+    $amount=$params['amount'];
+    $currencyId=$params['currencyId'];
+    $currencys=localAPI("GetCurrencies", [], flyfoxpay_wxpay_getAdminname());
+    if($currencys['result']=='success' and $currencys['totalresults']>=1){
+        
+    }else{
+        var_dump($currencys);
+        throw new \Exception('货币设置错误、API请求错误');
+        //如果api请求错误或者货币数量小于1
+    }
+    //获取货币。
+    $currencys=$currencys['currencies']['currency'];
+    foreach($currencys as $currency){
+        if($currencyId==$currency['id']){
+            $from=$currency;
+            break;
+        }
+    }
+    if(!$from){
+        throw new \Exception("货币错误，找不到起始货币。");
+    }
+    foreach($currencys as $currency){
+        $hb=strtoupper($currency['code']);
+        if($hb=='TWD'){
+            $cny=$currency;
+            break;
+        }
+    }
+    if(!$cny){
+        throw new \Exception("找不到新台币货币，请确认后台货币中存在货币代码为TWD的货币！");
+    }
+    $rate=$cny['rate']/$from['rate'];
+    return [round((double)$rate*$amount,2),round((double)$rate,2)];
+}
+}
+if(!function_exists("flyfoxpay_wxpay_getAdminname")){
+function flyfoxpay_wxpay_getAdminname(){
+    $admin = Capsule::table('tbladmins')->first();
+    return $admin->username;
+}
+}
